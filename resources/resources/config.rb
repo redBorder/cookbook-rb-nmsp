b763a6be70f05adbac6f8f9b7ac3478d9ef6239d
eareyes <eareyes@redborder.com>
2021-12-16 08:31:19 +0000

    First commit

---
+++ resources/resources/config.rb
@@ -0,0 +1,18 @@
+#Cookbook Name :: rbnmsp
+#
+# Resource:: config
+#
+
+actions :add, :remove , :register, :deregister
+default_action :add
+
+#TODO
+# Memory
+attribute :config_dir, :kind_of => String, :default => "/etc/redborder-nmsp"
+attribute :kafka_topic, :kind_of => String, :default => "rb_nmsp"
+attribute :name, :kind_of => String, :default => "localhost"
+attribute :ip, :kind_of => String, :default => "127.0.0.1"
+attribute :flow_nodes, :kind_of => Array, :default => []
+attribute :proxy_nodes, :kind_of => Object
+attribute :memory, :kind_of => Fixnum, :default => 524288
+
